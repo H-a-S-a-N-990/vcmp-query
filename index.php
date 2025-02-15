db00a5b53314c806dd53293e3f71c5afd3afcff8
H.a.S.a.N <138579037+H-a-S-a-N-990@users.noreply.github.com>
2025-02-15 11:07:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,5 @@
 require_once 'Server.php';
 
 // Create a new instance of the Server class with the IP and port
-$server = new Server("57.129.44.185", 8192);
-
-// Refresh the server information and player list
-$server->Refresh();
-
-// Output the server details
-if ($server->success) {
-    echo "Server Name: " . $server->name . "<br>";
-    echo "Version: " . $server->version . "<br>";
-    echo "Passworded: " . ($server->passworded ? "Yes" : "No") . "<br>";
-    echo "Players: " . $server->players . "/" . $server->max_players . "<br>";
-    echo "Player List: <br>";
-    print_r($server->player_list); // Display the player list
-} else {
-    echo "Failed to retrieve server information.";
-}
-?>
+$server = new Server("178.32.116.43", 4748);
+print_r($server);
