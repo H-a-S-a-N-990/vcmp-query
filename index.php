9f0733f4a717c8224cac3195d0d370e14cf25aa4
H.a.S.a.N <138579037+H-a-S-a-N-990@users.noreply.github.com>
2025-02-15 09:52:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
 
-require_once 'VcmpQueryAPI.php'; // Include the VCMP query class
+require_once 'VcmpQueryAPI.php';  // Make sure this file exists
 
-$query = new VcmpQueryAPI('57.129.44.185', 8192);
+$ip = '57.129.44.185';
+$port = 8192;
 
-if ($query->connect()) {
-    $info = $query->getInfo();
-    $players = $query->getPlayers();
+$query = new VcmpQueryAPI($ip, $port);
+$data = $query->getInfo();  // Use getInfo() instead of connect()
 
-    echo "<pre>";
-    print_r($info);
-    print_r($players);
-    echo "</pre>";
+if ($data) {
+    print_r($data);
 } else {
     echo "Failed to connect to the VCMP server.";
 }
-
 ?>
