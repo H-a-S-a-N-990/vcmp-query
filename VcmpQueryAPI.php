2c778ba72312696bd5823f1f37fcf4a8f84c0bbc
H.a.S.a.N <138579037+H-a-S-a-N-990@users.noreply.github.com>
2025-02-15 09:45:48 +0300

    Update VcmpQueryAPI.php

--- VcmpQueryAPI.php
+++ VcmpQueryAPI.php
@@ -1,237 +1,121 @@
 <?php
-/**
- * This API connects directly to the VC:MP server, without any need for any
- * middlemen connections.
- * Your server must have fsockopen enabled in order to access the 
- * functions that have been made available from this.
- *
- * @package vcmpAPI
- * @version 1.0
- * @author Your Name
- * @copyright 2025
- */
-
 class VcmpQueryAPI
 {
-    /**
-     * @ignore
-     */
     private $rSocket = false;
-    
-    /**
-     * @ignore
-     */
     private $aServer = array();
-    
-    /**
-     * Creation of the server class.
-     *
-     * @param string $sServer Server IP, or hostname.
-     * @param integer $iPort Server port
-     */
+
     public function __construct($sServer, $iPort = 8192)
     {
-        /* Fill some arrays. */
         $this->aServer[0] = $sServer;
         $this->aServer[1] = $iPort;
-        
-        /* Start the connection. */    
-        $this->rSocket = fsockopen('udp://'.$this->aServer[0], $this->aServer[1], $iError, $sError, 2);
-        
-        if(!$this->rSocket)
-        {
+
+        // Open a UDP socket
+        $this->rSocket = fsockopen('udp://' . $this->aServer[0], $this->aServer[1], $iError, $sError, 2);
+
+        if (!$this->rSocket) {
             $this->aServer[4] = false;
             return;
         }
-        
+
         socket_set_timeout($this->rSocket, 2);
-        
-        $sPacket = 'VCMP';
-        $sPacket .= chr(strtok($this->aServer[0], '.'));
-        $sPacket .= chr(strtok('.'));
-        $sPacket .= chr(strtok('.'));
-        $sPacket .= chr(strtok('.'));
-        $sPacket .= chr($this->aServer[1] & 0xFF);
-        $sPacket .= chr($this->aServer[1] >> 8 & 0xFF);
-        $sPacket .= 'i';
-        
-        fwrite($this->rSocket, $sPacket);
-        
-        if(fread($this->rSocket, 10))
-        {
-            if(fread($this->rSocket, 4) == 'VCMP')
-            {
-                $this->aServer[4] = true;
-                return;
-            }
-        }
-        
-        $this->aServer[4] = false;
+        $this->aServer[4] = true;
     }
-    
-    /**
-     * @ignore
-     */
+
     public function __destruct()
     {
         @fclose($this->rSocket);
     }
-    
-    /**
-     * Used to tell if the server is ready to accept queries.
-     *
-     * If false is returned, then it is suggested that you remove the
-     * class from active use, so that you can reload the class if needs
-     * be.
-     *
-     * @return bool true if success, false if failure.
-     */
+
     public function isOnline()
     {
         return isset($this->aServer[4]) ? $this->aServer[4] : false;
     }
-    
-    /**
-     * This function is used to get the server information.
-     *
-     * @return array Array of server information.
-     */
+
     public function getInfo()
     {
         @fwrite($this->rSocket, $this->createPacket('i'));
+        $response = fread($this->rSocket, 1024);
         
-        fread($this->rSocket, 11); // Read the header
+        if (!$response || strlen($response) < 28) {
+            return false;
+        }
 
-        $aDetails['password'] = (integer) ord(fread($this->rSocket, 1)); // Password protected?
-        
-        $aDetails['players'] = (integer) $this->toInteger(fread($this->rSocket, 2)); // Player count
-        
-        $aDetails['maxplayers'] = (integer) $this->toInteger(fread($this->rSocket, 2)); // Max players
-        
-        $iStrlen = ord(fread($this->rSocket, 4)); // Hostname length
-        if(!$iStrlen) return -1;
-        
-        $aDetails['hostname'] = (string) fread($this->rSocket, $iStrlen); // Hostname
-        
-        $iStrlen = ord(fread($this->rSocket, 4)); // Gamemode length
-        $aDetails['gamemode'] = (string) fread($this->rSocket, $iStrlen); // Gamemode
-        
-        $iStrlen = ord(fread($this->rSocket, 4)); // Version length
-        $aDetails['version'] = (string) fread($this->rSocket, $iStrlen); // Server version
-        
+        $aDetails = array();
+        $aDetails['version'] = trim(substr($response, 11, 12));
+        $aDetails['password'] = ord(substr($response, 23, 1));
+        $aDetails['players'] = $this->toInteger(substr($response, 24, 2));
+        $aDetails['maxplayers'] = $this->toInteger(substr($response, 26, 2));
+
+        $offset = 28;
+        $aDetails['hostname'] = $this->readString($response, $offset);
+        $aDetails['gamemode'] = $this->readString($response, $offset);
+        $aDetails['mapname'] = $this->readString($response, $offset);
+
         return $aDetails;
     }
-    
-    /**
-     * This function gets a basic list of all the players on the server.
-     *
-     * @return array Array of player information.
-     */
-    public function getBasicPlayers()
+
+    public function getPlayers()
     {
         @fwrite($this->rSocket, $this->createPacket('c'));
-        fread($this->rSocket, 11);
-        
-        $iPlayerCount = ord(fread($this->rSocket, 2));
-        $aDetails = array();
-        
-        if($iPlayerCount > 0)
-        {
-            for($iIndex = 0; $iIndex < $iPlayerCount; ++$iIndex)
-            {
-                $iStrlen = ord(fread($this->rSocket, 1));
-                $aDetails[] = array
-                (
-                    "nickname" => (string) fread($this->rSocket, $iStrlen),
-                    "score" => (integer) $this->toInteger(fread($this->rSocket, 4)),
-                );
-            }
+        $response = fread($this->rSocket, 3072);
+
+        if (!$response || strlen($response) < 13) {
+            return false;
         }
-        
+
+        $iPlayerCount = $this->toInteger(substr($response, 11, 2));
+        $aDetails = array();
+        $offset = 13;
+
+        for ($i = 0; $i < $iPlayerCount; $i++) {
+            $aDetails[] = $this->readString($response, $offset);
+        }
+
         return $aDetails;
     }
-    
-    /**
-     * This function gets a detailed list of all the players on the server.
-     *
-     * @return array Array of player information.
-     */
-    public function getDetailedPlayers()
+
+    public function getPing()
     {
-        @fwrite($this->rSocket, $this->createPacket('d'));
-        fread($this->rSocket, 11);
-    
-        $iPlayerCount = ord(fread($this->rSocket, 2));
-        $aDetails = array();
-        
-        for($iIndex = 0; $iIndex < $iPlayerCount; ++$iIndex)
-        {
-            $aPlayer['playerid'] = (integer) ord(fread($this->rSocket, 1));
-            
-            $iStrlen = ord(fread($this->rSocket, 1));
-            $aPlayer['nickname'] = (string) fread($this->rSocket, $iStrlen);
-            
-            $aPlayer['score'] = (integer) $this->toInteger(fread($this->rSocket, 4));
-            $aPlayer['ping'] = (integer) $this->toInteger(fread($this->rSocket, 4));
-            
-            $aDetails[] = $aPlayer;
-            unset($aPlayer);
-        }
-        
-        return $aDetails;
+        @fwrite($this->rSocket, $this->createPacket('p'));
+        return fread($this->rSocket, 1024);
     }
-    
-    /**
-     * @ignore
-     */
-    private function toInteger($sData)
-    {
-        if($sData === "")
-        {
-            return null;
-        }
-        
-        $iInteger = 0;
-        $iInteger += (ord($sData[0]));
- 
-        if(isset($sData[1]))
-        {
-            $iInteger += (ord($sData[1]) << 8);
-        }
-        
-        if(isset($sData[2]))
-        {
-            $iInteger += (ord($sData[2]) << 16);
-        }
-        
-        if(isset($sData[3]))
-        {
-            $iInteger += (ord($sData[3]) << 24);
-        }
-        
-        if($iInteger >= 4294967294)
-        {
-            $iInteger -= 4294967296;
-        }
-        
-        return $iInteger;
-    }
-    
-    /**
-     * @ignore
-     */
+
     private function createPacket($sPayload)
     {
-        $sPacket = 'VCMP';
-        $sPacket .= chr(strtok($this->aServer[0], '.'));
-        $sPacket .= chr(strtok('.'));
-        $sPacket .= chr(strtok('.'));
-        $sPacket .= chr(strtok('.'));
+        $sPacket = 'MP04';
+        $ipParts = explode('.', $this->aServer[0]);
+
+        if (count($ipParts) != 4) {
+            return false;
+        }
+
+        foreach ($ipParts as $part) {
+            $sPacket .= chr(intval($part));
+        }
+
         $sPacket .= chr($this->aServer[1] & 0xFF);
         $sPacket .= chr($this->aServer[1] >> 8 & 0xFF);
         $sPacket .= $sPayload;
-    
+
         return $sPacket;
+    }
+
+    private function toInteger($sData)
+    {
+        $iInteger = 0;
+        for ($i = 0; $i < strlen($sData); $i++) {
+            $iInteger |= ord($sData[$i]) << ($i * 8);
+        }
+        return $iInteger;
+    }
+
+    private function readString($sData, &$offset)
+    {
+        $length = $this->toInteger(substr($sData, $offset, 4));
+        $offset += 4;
+        $string = substr($sData, $offset, $length);
+        $offset += $length;
+        return trim($string);
     }
 }
 ?>
